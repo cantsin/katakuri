549f8b16e1eb6981bfa8fa0a0bc1867ff9c07613
James Tranovich <jtranovich@gmail.com>
2015-04-08 15:15:47 -0700

    Return websocket url from RTM initialization.

--- lib/rtm.ex
+++ lib/rtm.ex
@@ -3,7 +3,13 @@ defmodule Rtm do
 
   def start(token) do
     HTTPotion.start
-    response = HTTPotion.get (@url <> token)
-    response.body |> to_string |> :jsx.decode |> JSONMap.to_map
+    response = HTTPotion.get(@url <> token)
+    result = response.body |> to_string |> :jsx.decode |> JSONMap.to_map
+    %{ok: validated} = result
+    if validated do
+      Map.get(result, :url)
+    else
+      exit "Token is incorrect."
+    end
   end
 end
