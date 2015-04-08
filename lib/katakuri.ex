0f095708fd38106bd8a95ca6d3ddf741dd08cbef
James Tranovich <jtranovich@gmail.com>
2015-04-08 14:54:11 -0700

    Obtain RTM metadata.

--- lib/katakuri.ex
+++ lib/katakuri.ex
@@ -1,2 +1,3 @@
 defmodule Katakuri do
+
 end
--- lib/rtm.ex
+++ lib/rtm.ex
@@ -1,7 +1,9 @@
 defmodule Rtm do
-  @url "https://slack.com/api/rtm.start"
+  @url "https://slack.com/api/rtm.start?token="
 
   def start(token) do
-    url = @url <> token
+    HTTPotion.start
+    response = HTTPotion.get (@url <> token)
+    response.body |> to_string |> :jsx.decode |> JSONMap.to_map
   end
 end
