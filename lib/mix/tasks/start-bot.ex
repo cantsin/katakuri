9ab0120287120de7132cc85fcec4bdb110629262
James Tranovich <jtranovich@gmail.com>
2015-09-15 18:55:08 -0700

    Fixes for start-bot mix task.

--- lib/mix/tasks/start-bot.ex
+++ lib/mix/tasks/start-bot.ex
@@ -4,13 +4,19 @@ defmodule Mix.Tasks.StartBot do
 
   @shortdoc "Start a slack bot with the given API token."
 
+  def sleep do
+    :timer.sleep(1)
+    sleep
+  end
+
   def run(args) do
     Mix.Task.run "app.start", args
     {options, _, _} = OptionParser.parse args, switches: [token: :string], aliases: [t: :token]
-    if Keyword.get([], :token) do
+    if Keyword.get(options, :token) do
       token = Keyword.fetch!(options, :token)
       IO.puts "Connecting to Slack with the token '#{token}'"
       Katakuri.init token
+      sleep
     else
       IO.puts "Error: please pass in a token."
       IO.puts ""
